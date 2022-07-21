f85ad35ac7e30bd35e400e3e99f80678c3f6de19
xuekelindun <xuekelindun@kuaishou.com>
2022-07-21 14:44:43 +0800

    update

--- run-linux.sh
+++ run-linux.sh
@@ -4,7 +4,7 @@ runAS() {
     echo ">>>>>>>> 删除已存在的容器：test-as-vscode。"
     docker stop test-as-vscode && docker rm test-as-vscode
     echo ">>>>>>>> 加载镜像中，请耐心等待."
-    docker docker load --input as-vscode-dev-env-v7.tar 
+    docker load --input as-vscode-dev-env-v7.tar 
     echo ">>>>>>>> 开始运行新的容器：test-as-vscode."
     sudo docker-compose -f docker-compose.yml up -d
     echo ">>>>>>>> 容器运行完毕，进入初始化，请在初始化完毕之后，执行 exit 进行退出。"
